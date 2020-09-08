5c81920db05d998384944614bb1305404b6b7d61
rahulnarang21 <narangrahul.123@gmail.com>
2020-09-08 16:06:05 +0530

    Second Commit

--- app/src/main/java/goforfit/com/kotlinapp/MainActivity.kt
+++ app/src/main/java/goforfit/com/kotlinapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package goforfit.com.kotlinapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,12 +12,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        abc(5)
-        val b = BaseImpl(7)
-        Derived(b).printMe()
+//        abc(5)
+//        val b = BaseImpl(7)
+//        Derived(b).printMe()
 
         btnShowToast.setOnClickListener{
             Toast.makeText(this,"Hello World",Toast.LENGTH_LONG).show()
+        }
+
+        btnSendData.setOnClickListener {
+            //Toast.makeText(this,"Send Data clicked",Toast.LENGTH_LONG).show()
+            val message:String
+            message = edtMsg.text.toString() // in kotlin we have properties for every class, not functions
+            // so here text will work as getText()
+            Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+
         }
 
     }
